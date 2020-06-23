10dfb2bcb71f87f3c1d3d79a3b099dc4a5bea5e8
Renato Welinton Schlogel <renato.s@outlook.com>
2020-06-23 18:11:24 -0300

    ajustes

--- src/GeradorCmc7.js
+++ src/GeradorCmc7.js
@@ -1,13 +1,22 @@
 function gerarCmc7(){
   let numeroCheque = Math.floor(Math.random() * 999999)
-  const cmc7Modelo = "3129632d472nnnnnn5009241501056" ;
 
-  let cmc7Novo = cmc7Modelo.replace('nnnnnn', ("000000" + numeroCheque).slice(-6) );
-
+  let cmc7Novo = getModelo().replace('nnnnnn', ("000000" + numeroCheque).slice(-6) );
   const digito = calcularDigito(cmc7Novo);
   cmc7Novo = cmc7Novo.replace('d', digito);
 
   document.getElementById("campo-resultante").value = cmc7Novo;
+}
+
+
+
+function getModelo() {
+  const cmc7Modelo = [
+    "3129632d472nnnnnn5009241501056",
+    "7564238d033nnnnnn5900052800108"];
+
+  let index = Math.floor(Math.random() * 2);
+  return cmc7Modelo[index];
 }
 
 function calcularDigito(cmc7Modelo) {
@@ -31,5 +40,3 @@ function calcularDigito(cmc7Modelo) {
 
   return digito > 9 ? 0 : digito;
 }
-
-// module.exports = gerarCmc7;
